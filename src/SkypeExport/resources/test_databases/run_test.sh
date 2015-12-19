81eca7902630b6a992994f16b6c79642dabef0a9
Temptin <Temptin@users.noreply.github.com>
2015-12-20 00:31:26 +0100

    Corrected binary path in test-database script

--- src/SkypeExport/resources/test_databases/run_test.sh
+++ src/SkypeExport/resources/test_databases/run_test.sh
@@ -2,5 +2,5 @@
 
 for DB in *.db; do
 	DB=$(echo "${DB}" | sed "s/\.db$//")
-	../../_gccbuild/release/SkypeExport --db "${DB}.db" --outpath "./output/${DB}"
+	../../_gccbuild/macosx/release/SkypeExport --db "${DB}.db" --outpath "./output/${DB}"
 done
